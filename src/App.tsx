89f000c2de50d9cce3266d489719c60d7e79adf2
AmirSalem123 <aantunessalem@gmail.com>
2024-03-12 10:03:39 -0400

    Meu Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import '.App.css'
+import MeuComponente from './componentes/MeuComponente'
+
+function App() {
+  let nome = "Amir"
+  let sobrenome = "top";
+  return(
+    <>
+
+    <h1> Classname= "meu nome" salva -{nome} {sobrenome}</h1><h1>OLa eu sou o {nome} {sobrenome}</h1>
+    </>
+  ) //</h1>JSX - Javascript XML
+} 
+
+export default App 
---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,30 @@
+//Faça um componente que faça um retangulo da largura da tela 
+// por 200px
+
+//crie uma variavel com LET no js que contenha o valor "Meu Componente"
+
+// Crie uma div no componente e dentro dela mostre um <p>
+// contendo o valor da variavel.
+import './MeuComponente.css'
+function MeuComponente(){
+    let variavel = "Meu Componente"
+    return(
+        <div className="minha-div"></p>{variavel}</p>
+        </div>
+        
+        
+        
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default "MeuComponente";
